582d4bc932b8398ee496e4390bb1ae3abb384d89
Jaime Velez <jandres.vr@hotmail.com>
2019-02-02 13:15:54 -0500

    setting the index.js to can use redux dev-tool extension

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+);
 
 const store = createStore(
   reducers,
-  applyMiddleware(thunk)
+  enhancer
 );
 
 ReactDOM.render(
