62a34fe6ffac2eaccff2abe4a9d86da6f3de945c
Jaime Velez <jandres.vr@hotmail.com>
2019-02-02 23:38:06 -0500

    new design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
-import Login from './containers/login';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      pass: ''
+    }
+  }
   render() {
     return (
       <div>
         <div className="header"></div>
-        <div>
-          <Login />
+        <div className="box">
+          <div className="row">
+            <div className="aside"></div>
+            <div className="content">
+              <form className="forms">
+                <h3>E-mail</h3>
+                <input
+                  className="inputs"
+                  type="email"
+                  placeholder="email"
+                  onChange={e => this.setState({ email: e.target.value })} />
+                <h3>Password</h3>
+                <input
+                  className="inputs"
+                  type="password"
+                  placeholder="password"
+                  onChange={e => this.setState({ pass: e.target.value })} />
+                <br></br>
+                <button
+                  className="green"
+                  disabled={this.state.email.length ===0 || this.state.pass.length ===0}
+                >Sign In</button>
+              </form>
+            </div>
+            <div className="aside"></div>
+          </div>
         </div>
-        <div className="footer"></div>
       </div>
     );
   }
